234f9d80b4c887dbf0a121e89f6ba951173cd9cc
Sergii Glushchenko <gsserge@amazon.com>
2022-02-02 13:12:08 +0100

    Fix warnings, adjust coverage and add .gitignore
    Signed-off-by: Sergii Glushchenko <gsserge@amazon.com>

--- src/bindings_v5_0_0/mod.rs
+++ src/bindings_v5_0_0/mod.rs
@@ -5,6 +5,8 @@
 #![allow(non_upper_case_globals)]
 #![allow(non_camel_case_types)]
 #![allow(non_snake_case)]
+// Keep this until https://github.com/rust-lang/rust-bindgen/issues/1651 is fixed.
+#![cfg_attr(test, allow(deref_nullptr, unaligned_references))]
 
 pub mod virtio_blk;
 pub mod virtio_net;
